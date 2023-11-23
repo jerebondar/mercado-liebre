4b819ec943b0bfd827e3e9de5231d38b437789e8
jerebondar <gere.bond.ar@gmail.com>
2023-11-23 15:44:02 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.use(express.static("public"));
+
+
+app.listen(3000, () => {
+    console.log("Servidor funcionando")
+})
+
+app.get("/", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/home.html")
+    res.sendFile(htmlPath)
+})
+
+app.get("/creatucuenta", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/register.html")
+    res.sendFile(htmlPath)
+})
+
+app.get("/ingresa", (req, res) => {
+    let htmlPath = path.resolve(__dirname, "./views/login.html")
+    res.sendFile(htmlPath)
+})
+
