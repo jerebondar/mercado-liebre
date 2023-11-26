cd5f8956eb55c35f7cf7fa56b066677375b9c96a
jerebondar <gere.bond.ar@gmail.com>
2023-11-26 18:57:28 -0300

    Codigos necesarios para subir la pagina a internet

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const path = require("path");
 app.use(express.static("public"));
 
 
-app.listen(3000, () => {
+/* app.listen(3000, () => {
     console.log("Servidor funcionando")
-})
+}) */
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>console.log('Servidor corriendo'))
 
 app.get("/", (req, res) => {
     let htmlPath = path.resolve(__dirname, "./views/home.html")
